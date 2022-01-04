86389c6fb9c1dfe71f0bd7b75d872994386d84bb
Aisha Sabrie <sabriealt@gmail.com>
2022-01-04 18:25:33 -0500

    Refactor query

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -1,12 +1,7 @@
-SELECT assignments.id as id, assignments.name as name, day, chapter, total_requests
+SELECT assignments.id as id, assignments.name as name, day, chapter, count(assistance_requests) as total_requests
 FROM assignments
-JOIN 
-(SELECT COUNT(assistance_requests.*) as total_requests, assignment_id
-FROM assistance_requests
-JOIN assignments
-ON assignment_id = assignments.id
-GROUP BY assignment_id) as new_table
-ON new_table.assignment_id = assignments.id
+JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
 ORDER BY total_requests DESC;
 
 
